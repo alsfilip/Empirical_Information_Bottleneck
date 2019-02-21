f608498838b827f780d954a0df6e560fcb12f1c6
Alexandre Filipowicz <alsfilip@modv-ve502-0650.apn.wlan.upenn.edu>
2019-02-21 14:21:27 -0500

    Trying to get better bound

--- cluster_EB_main.py
+++ cluster_EB_main.py
@@ -126,7 +126,7 @@ def main():
     read_data = np.load('data/DAW_DATA_FOR_CLUSTER.npy').item()
     keys = ['S2', 'R1', 'R2', 'Rw', 'best_R1']
     #keys = ['S2','R1','R2','Rw'] #smaller size to test
-    parallel = 8 #AF: change this to an integer to specify the number of cores available - False if you don't want to use parallel processing
+    parallel = 16 #AF: change this to an integer to specify the number of cores available - False if you don't want to use parallel processing
 
     # THIS IS MAIN LOOP OVER different models or weighting params
     print('COMPUTING BOUND')
@@ -150,7 +150,7 @@ def main():
         Ffuture = features[window_size:]
         """
 
-        i_p_emp,i_f_emp,beta,mi1,hx,hy = eb(Fpast, Ffuture, numbeta=3000, maxbeta=2000,parallel = parallel) #AF: parallel distributes the beta calculations across the number of specified cores
+        i_p_emp,i_f_emp,beta,mi1,hx,hy = eb(Fpast, Ffuture, numbeta=10000, maxbeta=2000,parallel = parallel) #AF: parallel distributes the beta calculations across the number of specified cores
 
 
 
@@ -162,14 +162,14 @@ def main():
         np.save('./MBMF_bounds/EIB_W_%f'%w,hull)
     
         #Plot the hull
-        plt.figure()
-        #plt.plot(hull[0:-1,0],hull[0:-1,1],'-o')
-        plt.plot(hull[0:-1,0],hull[0:-1,1],'-')
-        plt.plot(hull[0:-1,0],hull[0:-1,1],'ok')
-        plt.title('empirical bound for w = %.2f' % w)
-        plt.ylabel('$I_{future}$')
-        plt.xlabel('$I_{past}$')
-        plt.savefig('./BoundFigs/MBMF_EIB_W_%f.pdf'%w)
+#         plt.figure()
+#         #plt.plot(hull[0:-1,0],hull[0:-1,1],'-o')
+#         plt.plot(hull[0:-1,0],hull[0:-1,1],'-')
+#         plt.plot(hull[0:-1,0],hull[0:-1,1],'ok')
+#         plt.title('empirical bound for w = %.2f' % w)
+#         plt.ylabel('$I_{future}$')
+#         plt.xlabel('$I_{past}$')
+#         plt.savefig('./BoundFigs/MBMF_EIB_W_%f.pdf'%w)
     print('\nFINISHED')
 
 if __name__ == '__main__':
