1a9676183c6a205a4f2ac59b16efd96da87319dc
Alex Filipowicz <alsfilip@gmail.com>
2019-02-26 09:05:37 -0500

    Max beta = 1

--- cluster_EB_main.py
+++ cluster_EB_main.py
@@ -152,7 +152,7 @@ def main():
         Ffuture = features[window_size:]
         """
 
-        i_p_emp,i_f_emp,beta,mi1,hx,hy = eb(Fpast, Ffuture, numbeta=200000, maxbeta=.7,parallel = parallel) #AF: parallel distributes the beta calculations across the number of specified cores
+        i_p_emp,i_f_emp,beta,mi1,hx,hy = eb(Fpast, Ffuture, numbeta=50000, maxbeta=1.2,parallel = parallel) #AF: parallel distributes the beta calculations across the number of specified cores
 
 
 
